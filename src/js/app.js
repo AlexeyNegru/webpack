8a4311254afb4bd87039a60215149dc4f41196a1
Alex n <Shakal240@gmail.com>
2023-06-26 17:34:04 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// TODO: write your code here
+import loadGame, * as saveGame from './domain.js';
+console.log(loadGame, saveGame);
+const game = new Game();
+game.start();
+
+console.log('app worked');
+
+// console.log(sum([1, 2]));
---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+import Character from './domain.js';
+console.log(Character);
+export default Game; GameSavingData, readGameSaving, writeGameSaving;
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
